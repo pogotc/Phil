907e3a428f93d06ea964896ba77a31f7914d2b8a
Steven Ellis <steven.ellis@mademedia.co.uk>
2015-05-24 11:28:28 +0100

    Tweaked test so it passes a tokeniser to the Phil class

--- tests/MathsTest.php
+++ tests/MathsTest.php
@@ -1,15 +1,23 @@
 <?php
 
 use Pogotc\Phil\Phil;
+use Pogotc\Phil\Tokeniser;
 
 class MathsTest extends \PHPUnit_Framework_TestCase
 {
 
+    private $phil;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->phil = new Phil(new Tokeniser());
+    }
+
     public function testAddition()
     {
         $input = '(+ 2 2)';
-        $phil = new Phil();
-        $this->assertEquals(4, $phil->run($input));
+        $this->assertEquals(4, $this->phil->run($input));
     }
 
 }
