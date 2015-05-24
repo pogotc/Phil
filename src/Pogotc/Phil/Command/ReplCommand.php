8fee5391aa94cff538db2d5c8951bb412e446460
Steven Ellis <steven.ellis@mademedia.co.uk>
2015-05-24 23:49:38 +0100

    Added bin file / simple REPL

---
+++ src/Pogotc/Phil/Command/ReplCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Pogotc\Phil\Command;
+
+use Pogotc\Phil\Evaluator;
+use Pogotc\Phil\Parser;
+use Pogotc\Phil\Phil;
+use Pogotc\Phil\Scope;
+use Pogotc\Phil\Tokeniser;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReplCommand extends Command
+{
+
+    protected function configure()
+    {
+        $this->setName('repl')
+             ->setDescription('Run the PHIL REPL');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $phil = new Phil(new Tokeniser(), new Parser(), new Evaluator(new Scope()));
+
+        do {
+            $line = readline("Parser> ");
+            $output->writeln($phil->run($line));
+        } while (true);
+    }
+}
