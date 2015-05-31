83661802f57b265e2895329785a0d8feb2fe9bb9
Steven Ellis <steven.ellis@mademedia.co.uk>
2015-05-31 22:23:19 +0100

    Added in a couple more tests for conditionals

--- tests/ConditionalTest.php
+++ tests/ConditionalTest.php
@@ -8,5 +8,7 @@ class ConditionalTest extends AbstractPhilTest
     {
         $this->assertEquals("a", $this->phil->run('(if true "a" "b")'));
         $this->assertEquals("b", $this->phil->run('(if false "a" "b")'));
+        $this->assertEquals("a", $this->phil->run('(if (= 5 5) "a" "b")'));
+        $this->assertEquals(50, $this->phil->run('(if (= 5 5) (* 5 10) (+ 5 10))'));
     }
 }
